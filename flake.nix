b6143ca32c106284381251e6e074f74e556c21cd
Ryan Gibb <ryan@freumh.org>
2026-03-13 13:14:19 +0000

    fix dev shell missing Haskell deps by using shellFor instead of ghcWithHoogle

--- flake.nix
+++ flake.nix
@@ -14,12 +14,6 @@
         # Build the meur package from the cabal file
         meur = pkgs.haskellPackages.callCabal2nix "meur" ./. { };
 
-        # Haskell development environment with tools
-        haskell-env = pkgs.haskellPackages.ghcWithHoogle (hp: with hp; [
-          haskell-language-server
-          cabal-install
-        ] ++ meur.buildInputs);
-
       in
       {
         packages = {
@@ -32,20 +26,19 @@
           program = "${meur}/bin/meur";
         };
 
-        devShells.default = pkgs.mkShell {
-          name = "meur-dev";
+        devShells.default = pkgs.haskellPackages.shellFor {
+          packages = p: [ meur ];
+          withHoogle = true;
+          nativeBuildInputs = with pkgs.haskellPackages; [
+            haskell-language-server
+            cabal-install
+          ];
           buildInputs = [
-            haskell-env
             pkgs.imagemagick  # For thumbnail generation
             pkgs.curl         # For geocoding
           ];
           shellHook = ''
             export HAKYLL_ENV="development"
-            export HIE_HOOGLE_DATABASE="${haskell-env}/share/doc/hoogle/default.hoo"
-            export NIX_GHC="${haskell-env}/bin/ghc"
-            export NIX_GHCPKG="${haskell-env}/bin/ghc-pkg"
-            export NIX_GHC_DOCDIR="${haskell-env}/share/doc/ghc/html"
-            export NIX_GHC_LIBDIR=$( $NIX_GHC --print-libdir )
 
             echo "Meur development environment loaded!"
             echo "Available commands:"
